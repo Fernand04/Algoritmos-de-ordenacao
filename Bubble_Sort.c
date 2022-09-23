c9b34ba764d9f36c0e2cc021f2e3d2e495c87736
Fernand04 <106198262+Fernand04@users.noreply.github.com>
2022-09-23 07:58:01 -0300

    Update Bubble_Sort.c

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -10,9 +10,6 @@ int main() {
     // Percorre o vetor a partir do primeiro até o penúltimo elemento do vetor, e a cada ciclo compara o elemento atual com o próximo e os troca de posição se caso o atual seja maior do que o próximo.
     
     for(int k=0; k<n-1; k++){
-
-      // A troca é feita da seguinte forma: a váriavel auxiliar recebe o valor que está em v[k+1] para que ele não seja perdido durante a operação de troca, em seguida o vetor na posição v[k+1] recebe o valor que está em v[k], e por fim v[k] recebe o valor armazenado na váriavel auxiliar.
-
       if(v[k]>v[k+1]){
         aux = v[k+1];
         v[k+1] = v[k];
